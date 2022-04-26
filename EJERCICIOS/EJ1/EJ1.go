3bc08d658829c69ad6a6e95f44c90190f4155bbf
francopianca <piancatellifranco@gmail.com>
2022-04-26 19:02:41 -0300

    EJ 1 y 2

--- EJERCICIOS/EJ1/EJ1.go
+++ EJERCICIOS/EJ1/EJ1.go
@@ -1,13 +1,25 @@
-package div
+package main
 
 import (
 	"errors"
+	"fmt"
 )
 
-///
-func Division(a float64, b float64) (float64, error) {
+func division(a, b float64) (float64, error) {
+
 	if b == 0 {
-		return -1, errors.New("invalid zero divisor")
+		return -1, errors.New("MAL")
 	}
 	return a / b, nil
+}
+
+func main() {
+
+	err, div := division(15)
+	if err != nil {
+		fmt.Println("Error", err.Error())
+		return
+	}
+	fmt.Println("Division:", div)
+
 }
--- EJERCICIOS/EJercicios API/EJ1.go
+++
@@ -1,1 +0,0 @@
-package EJercicios_API
